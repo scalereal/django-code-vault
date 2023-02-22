0b3b79617f0ea6083cd71fe77d13cec7a4999a29
mr-meetpatel <pm2410.ldrp@gmail.com>
2023-02-22 09:21:02 +0530

    Update wsgi config

--- django_code_vault/config/wsgi.py
+++ django_code_vault/config/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_code_vault.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_code_vault.config.settings.django')
 
 application = get_wsgi_application()
